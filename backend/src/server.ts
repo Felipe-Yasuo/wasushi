567c935c4fed71d53d443f8246ca65c7f55f49c1
Felipe-Yasuo <felipe.yasuo@hotmail.com>
2025-10-29 18:10:28 -0300

    feat: adiciona middlewares globais e tratamento de erros no servidor

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,25 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from 'express'
+import cors from 'cors'
 import { router } from "./routes";
 
 const app = express();
 
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
+app.use(router)
 
-app.use(router);
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    console.error('🔥 Erro capturado:', err)
+
+    if (err instanceof Error) {
+        return res.status(400).json({ error: err.message })
+    }
+
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal Server Error',
+    })
+})
 
 const PORT = process.env.PORT || 3333;
-
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
