0f39b2af48709b1fa3ed2d077fd946ff151487bf
Felipe-Yasuo <felipe.yasuo@hotmail.com>
2025-10-29 18:38:30 -0300

    feat: integra Prisma ORM com PostgreSQL e adiciona schema inicial do banco

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,8 +8,10 @@ app.use(cors())
 app.use(express.json())
 app.use(router)
 
+
+// Middleware global de tratamento de erros
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-    console.error('🔥 Erro capturado:', err)
+    console.error('Erro capturado:', err)
 
     if (err instanceof Error) {
         return res.status(400).json({ error: err.message })
@@ -20,6 +22,8 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
         message: 'Internal Server Error',
     })
 })
+
+
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
