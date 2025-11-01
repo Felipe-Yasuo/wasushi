4c7823b4dd12bde27bcdaef04790c2204581b516
Felipe-Yasuo <felipe.yasuo@hotmail.com>
2025-11-01 18:53:18 -0300

    feat(user): adiciona rota /me e estrutura inicial de detalhe do usuário

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
+import { DetailuserController } from "./controllers/user/DetailUserController";
 
 const router = Router();
 
@@ -13,5 +14,6 @@ const authUserController = new AuthUserController();
 // Rotas
 router.post('/users', (req, res) => createUserController.handle(req, res))
 router.post("/session", (req, res) => authUserController.handle(req, res));
+router.get('/me', new DetailuserController().handle)
 
 export { router };
