88141f3ed70025a037608c7e62998e8b1b64ba7d
Felipe-Yasuo <felipe.yasuo@hotmail.com>
2025-11-01 19:04:04 -0300

    feat(auth): cria middleware isAuthenticated e protege rota /me com JWT

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailuserController } from "./controllers/user/DetailUserController";
+import { isAuthenticated } from "./middleware/isAuthenticated";
 
 const router = Router();
 
@@ -14,6 +15,9 @@ const authUserController = new AuthUserController();
 // Rotas
 router.post('/users', (req, res) => createUserController.handle(req, res))
 router.post("/session", (req, res) => authUserController.handle(req, res));
-router.get('/me', new DetailuserController().handle)
+
+
+
+router.get("/me", isAuthenticated, new DetailuserController().handle);
 
 export { router };
