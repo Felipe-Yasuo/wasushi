e0f010dc277056541fd6578a995d4af7ee543768
Felipe-Yasuo <felipe.yasuo@hotmail.com>
2026-03-30 12:29:55 -0300

    feat: add middleware for user authentication, admin role verification, and schema validation

---
+++ backend/src/middlewares/isAdmin.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from "express";
+import prismaClient from "../prisma/index"
+
+export const isAdmin = async (request: Request, response: Response, next: NextFunction): Promise<void> => {
+
+    const user_id = request.user_id;
+
+    if (!user_id) {
+        response.status(401).json({
+            error: "Usuário sem permissão"
+        });
+        return;
+    }
+
+    const user = await prismaClient.user.findFirst({
+        where: {
+            id: user_id
+        }
+    })
+
+    if (!user) {
+        response.status(401).json({
+            error: "Usuário sem permissão"
+        })
+        return;
+    }
+
+    if (user.role !== "ADMIN") {
+        response.status(401).json({
+            error: "Usuário sem permissão"
+        })
+        return;
+    }
+
+    next();
+}
